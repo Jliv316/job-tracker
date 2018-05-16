d927902d6c8a4bca5f1430075d013f68f14f2120
John Livingston <jclivingston316@gmail.com>
2018-05-16 09:58:11 -0600

    adds dashboard controller, model, view, route

--- spec/features/analysis/user_can_see_list_of_jobs_by_interest_spec.rb
+++ spec/features/analysis/user_can_see_list_of_jobs_by_interest_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe 'User visits /jobs?sort=location' do
-  scenario "user sees all the jobs sorted by city" do
+describe 'User visits /dashboard' do
+  scenario "user sees all the jobs sorted by level of interest" do
     company = Company.create!(name: "ESPN")
     category = Category.create!(title: 'Developer')
     job1 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category_id: category.id)
@@ -9,11 +9,11 @@ describe 'User visits /jobs?sort=location' do
     job3 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Chicago", category_id: category.id)
     job4 = company.jobs.create!(title: "QA Analyst", level_of_interest: 70, city: "Dallas", category_id: category.id)
     
-    visit '/jobs?sort=location'
+    visit '/dashboard'
 
-    expect(page).to have_content(job1.city)
-    expect(page).to have_content(job2.city)
-    expect(page).to have_content(job3.city)
-    expect(page).to have_content(job4.city)
+    expect(page).to have_content(job1.level_of_interest)
+    expect(page).to have_content(job2.level_of_interest)
+    expect(page).to have_content(job3.level_of_interest)
+    expect(page).to have_content(job4.level_of_interest)
   end
 end
