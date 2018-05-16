34aedd7b3e50f90d1df09134750d638ea232b1ac
John Livingston <jclivingston316@gmail.com>
2018-05-16 11:44:12 -0600

    tests all passing

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,7 @@
 class DashboardController < ApplicationController
   def index
-      @level_of_interest = Job.by_level_of_interest
-      @jobs_by_location = Job.by_location
-      render :index
-    end
+    @level_of_interest = Job.by_level_of_interest
+    @job_locations = Job.by_location
+    render :index
   end
 end
--- spec/features/analysis/user_can_view_top_3_companies_by_level_of_interest_spec.rb
+++ spec/features/analysis/user_can_view_top_3_companies_by_level_of_interest_spec.rb
@@ -16,13 +16,40 @@ describe 'User visits /dashboard' do
     job7 = company4.jobs.create!(title: "Developer", level_of_interest: 90, city: "Chicago", category_id: category.id)
     job8 = company4.jobs.create!(title: "QA Analyst", level_of_interest: 85, city: "Dallas", category_id: category.id)
 
-    visit '/jobs?location=Denver'
-
+    visit '/dashboard'
+    save_and_open_page
     expect(page).to have_content(job1.title)
     expect(page).to have_content(job2.title)
     expect(page).to have_content(job5.title)
     # expect(page).to have_content(company1.level_of_interest)
     # expect(page).to have_content(company3.level_of_interest)
     # expect(page).to have_content(company4.level_of_interest)
+  end
+
+  describe 'user visits /jobs?location=Denver' do
+    scenario "user sees all the jobs for specific city" do
+      company1 = Company.create!(name: "ESPN")
+      company2 = Company.create!(name: "Turing")
+      company3 = Company.create!(name: "Google")
+      company4 = Company.create!(name: "Apple")
+      category = Category.create!(title: 'Developer')
+      job1 = company1.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category_id: category.id)
+      job2 = company1.jobs.create!(title: "QA Analyst", level_of_interest: 40, city: "Denver", category_id: category.id)
+      job3 = company2.jobs.create!(title: "Developer", level_of_interest: 20, city: "Chicago", category_id: category.id)
+      job4 = company2.jobs.create!(title: "QA Analyst", level_of_interest: 0, city: "Dallas", category_id: category.id)
+      job5 = company3.jobs.create!(title: "Developer", level_of_interest: 90, city: "Denver", category_id: category.id)
+      job6 = company3.jobs.create!(title: "QA Analyst", level_of_interest: 30, city: "New York City", category_id: category.id)
+      job7 = company4.jobs.create!(title: "Developer", level_of_interest: 90, city: "Chicago", category_id: category.id)
+      job8 = company4.jobs.create!(title: "QA Analyst", level_of_interest: 85, city: "Dallas", category_id: category.id)
+
+      visit '/jobs?location=Denver'
+
+      expect(page).to have_content(job1.title)
+      expect(page).to have_content(job2.title)
+      expect(page).to have_content(job5.title)
+      # expect(page).to have_content(company1.level_of_interest)
+      # expect(page).to have_content(company3.level_of_interest)
+      # expect(page).to have_content(company4.level_of_interest)
+    end
   end
 end
